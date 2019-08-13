626f724782b1e60203c25592bec56a4f9356ae49
Saadat Faiz <saadatfaiz@gmail.com>
2019-08-13 18:59:10 -0400

    still setting up

---
+++ LeetCode/TwoSum.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    // complete this
+}
+
+int main() {
+    cout << "hello world" << endl;
+
+    return 0;
+}
